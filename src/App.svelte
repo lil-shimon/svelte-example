92784a10de480e261149f34195c5d40874df6b88
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-04-10 20:26:52 +0900

    feat: 🎸 add task

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,20 @@
 <script>
+// Todo
 let todos = [
 	{id: 0, done: false, title: 'レストランを予約する'},
 	{id: 1, done: false, title: 'サプライズ用の指輪を買う'},
 	{id: 2, done: false, title: 'スポーツをする'}
 ]
+
+// Todo Title
+let title = ''
+
+const add = () => {
+	todos = [
+		...todos,
+		{id: todos.length, done: false, title}
+	]
+}
 </script>
 
 <div>
@@ -13,12 +24,14 @@ let todos = [
 	<button>完了</button>
 </div>
 <div>
-	<input type="text">
-	<button>タスク追加</button>
+	<!-- input入力値とtitleを紐付ける -->
+	<input type="text" bind:value={title}>
+	<button on:click={() => add()}>タスク追加</button>
 </div>
 <div>
 	<ul>
 		<!-- foreach -->
+		<!-- (todo.id)と書くことでTodoとIdを紐付ける -> 変化を検知できる -->
 		{#each todos as todo (todo.id)}
 		<li>
 			<input type="checkbox" bind:checked={todo.done}> {todo.title}
